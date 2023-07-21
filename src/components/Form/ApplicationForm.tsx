7415750d5681bb16bab76ecf8e80f0dce51ddb9c
Wesley <wesleyadwebster@gmail.com>
2023-07-21 15:41:04 -0500

    Form layout work of RHF logic. Made interface / custom type

--- src/components/Form/ApplicationForm.tsx
+++ src/components/Form/ApplicationForm.tsx
@@ -2,20 +2,48 @@ import { useForm, SubmitHandler } from "react-hook-form";
 import classes from './ApplicationForm.module.css';
 
 type Inputs = {
-    example: string
-    exampleRequired: string
+    firstName: string;
+    lastName: string;
+    location: string;
+    email: string;
+    phoneNumber: number;
+    resume: FileList;
+    coverLetter?: FileList;
+    reason: string;
+    hearAboutJob: string;
+    isAuthorized: boolean;
+    socialMediaLinkedIn?: string;
+    socialMediaWebsite?: string;
+    stackOverflowLink?: string;
+    GithubLink?: string;
 };
 
 const { formContainer } = classes;
 
 const ApplicationForm = ({ }) => {
+
+    const { register, handleSubmit, watch, formState: { errors }, } = useForm<Inputs>();
+
+    const onSubmit: SubmitHandler<Inputs> = (data) => {
+        console.log(data);
+        console.log(data.resume[0].name);
+    } 
+
+    // console.log(watch("example")) // watch input value by passing the name of it
+
     return (
-        <form className={formContainer}>
-            <h2>Form</h2>
+        <form
+            className={formContainer}
+            onSubmit={handleSubmit(onSubmit)}
+        >
+            <h2>Applicant Form</h2>
             <section>
                 <label>First Name</label>
-                <input placeholder='Solid' type="text" />
+                <input {...register("firstName", { required: true })} />
                 <label>Second Name</label>
+                <input {...register("lastName", { required: true })} />
+                <input id="upload" {...register('resume')} type="file" />
+                <button type="submit">Submit</button>
 
             </section>
         </form>
