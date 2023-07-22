006622f2bc9c8e114d06f8b0f457cb326fa98dd4
Wesley <wesleyadwebster@gmail.com>
2023-07-22 06:30:01 -0500

    Application form error handling set and styling of layout

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+### ABOUT
+This is just a mock interface built with Vite using React Hook Forms. Includes error validation. You can see your results of the field after filling out the form, hitting submit and checking the devtools as it logs your information. Nothing is getting sent as there is no backend. I did not have access to Discord specific fonts, nor design this for mobile as it is not a good experience to upload and apply to jobs on mobile. 
+
+Purely for demo purposes and to show a better layout of the application than the current. With hopes of it being seen by a recruiter and / or fixing the bug on their form.
+ 
+**This is in no way affiliated with Discord officially.
+
 # React + TypeScript + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- src/components/Form/ApplicationForm.tsx
+++ src/components/Form/ApplicationForm.tsx
@@ -5,18 +5,19 @@ type Inputs = {
     firstName: string;
     lastName: string;
     location: string;
-    email: string;
     phoneNumber: number;
+    email: string;
     resume: FileList;
     coverLetter?: FileList;
+    isAuthorized: boolean;
     reason: string;
     hearAboutJob: string;
-    isAuthorized: boolean;
     socialMediaLinkedIn?: string;
     socialMediaWebsite?: string;
     stackOverflowLink?: string;
     GithubLink?: string;
 };
+
 
 const { formContainer } = classes;
 
@@ -27,7 +28,22 @@ const ApplicationForm = ({ }) => {
     const onSubmit: SubmitHandler<Inputs> = (data) => {
         console.log(data);
         console.log(data.resume[0].name);
-    }
+        const numFilledFields = countElements(data);
+        console.log(`Number of filled fields: ${numFilledFields}`);
+
+    };
+
+    const countElements = (el: Record<string, any>) => {
+        let total: number = 0;
+
+        for (let obj in el) {
+            if (el[obj]) { // Only increment total if the value is truthy
+                total += 1;
+            }
+        };
+
+        return total;
+    };
 
     // console.log(watch("example")) // watch input value by passing the name of it
 
@@ -38,59 +54,100 @@ const ApplicationForm = ({ }) => {
         >
             <h2>Applicant Form</h2>
             <h4>Contact Information</h4>
-            <p>Fill out the information below</p>
+            <p>Fill out the information below. Resume and cover letters should be in PDF format.</p>
             <section className='contact-information-section'>
                 <div>
-                    <label>First Name</label>
-                    <input {...register("firstName", { required: true })} />
+                    <label><span>*</span>First Name</label>
+                    <input {...register("firstName", { required: "Field cannot be blank" })} />
+                    {errors.firstName && <span> {errors.firstName.message} </span>}
                 </div>
                 <div>
-                    <label>Last Name</label>
-                    <input {...register("lastName", { required: true })} />
+                    <label><span>*</span>Last Name</label>
+                    <input {...register("lastName", { required: "Field cannot be blank" })} />
+                    {errors.lastName && <span> {errors.lastName.message} </span>}
                 </div>
                 <div>
-                    <label>Resume</label>
-                    <input id="upload" {...register('resume')} type="file" />
+                    <label><span>*</span>Resume</label>
+                    <input id="upload" {...register('resume', { required: "A Resume must be uploaded" })} type="file" />
+                    {errors.resume && <span> {errors.resume.message} </span>}
                 </div>
             </section>
             <section className="contact-information-section">
                 <div>
-                    <label>Location</label>
-                    <input {...register("location", { required: true })} />
+                    <label><span>*</span>Location</label>
+                    <input {...register("location", { required: "Location required" })} />
+                    {errors.location && <span> {errors.location.message} </span>}
                 </div>
                 <div>
-                    <label>Phone Number</label>
-                    <input {...register("phoneNumber", { required: true })} />
+                    <label><span>*</span>Phone Number</label>
+                    <input {...register("phoneNumber", { required: "A valid phone number must be entered." })} />
+                    {errors.phoneNumber && <span> {errors.phoneNumber.message} </span>}
                 </div>
                 <div>
                     <label>Cover Letter</label>
                     <input id="upload" {...register('coverLetter')} type="file" />
                 </div>
                 <div>
-                    <label>Email Address</label>
-                    <input {...register("email", { required: true })} />
+                    <label><span>*</span>Email Address</label>
+                    <input
+                        {...register("email", {
+                            required: "Field cannot be blank",
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                message: "A valid email address is required"
+                            }
+                        })}
+                    />
+                    {errors.email && <span> {errors.email.message} </span>}
                 </div>
             </section>
             <section>
                 <h4>Job Logistics</h4>
-                {/* TODO: Fix for radio button and text area. */}
                 <div>
-                    <label>Are you legally authorized to work in the United States for our company?*</label>
+                    <label><span>*</span>Are you legally authorized to work in the United States for our company?</label>
                     <div className="legal-question-section">
-                        <input {...register('isAuthorized')} type="radio" value="yes" />
+                        <input {...register('isAuthorized', { required: "An answer is required for this field." })} type="radio" value="yes" />
                         <label>Yes</label>
                     </div>
                     <div className="legal-question-section">
-                        <input {...register('isAuthorized')} type="radio" value="no" />
+                        <input {...register('isAuthorized', { required: "An answer is required for this field." })} type="radio" value="no" />
                         <label>No</label>
                     </div>
+                    {errors.isAuthorized && <span> {errors.isAuthorized.message} </span>}
+                </div>
+                <div id="reasonBlock">
+                    <label><span>*</span>Why do you want to work at Discord?</label>
+                    <textarea {...register("reason", { required: "Field cannot be blank." })} />
+                    {errors.reason && <span> {errors.reason.message} </span>}
                 </div>
                 <div>
-                    <label>Reason</label>
-                    <textarea {...register("reason", { required: true })} />
+                    <label><span>*</span>How did you hear about this job?</label>
+                    <input {...register('hearAboutJob', { required: "Field cannot be blank" })} />
+                    {errors.hearAboutJob && <span> {errors.hearAboutJob.message} </span>}
                 </div>
             </section>
-            <button type="submit">Submit</button>
+            <h4>Social Links (LinkedIn, GitHub, Portfolio)</h4>
+            <section className="social-links-container">
+                <div>
+                    <label>LinkedIn</label>
+                    <input {...register("socialMediaLinkedIn")} />
+                </div>
+                <div>
+                    <label>Website</label>
+                    <input {...register("socialMediaWebsite")} />
+                </div>
+                <div>
+                    <label>Stack Overflow</label>
+                    <input {...register("stackOverflowLink")} />
+                </div>
+                <div>
+                    <label>GitHub</label>
+                    <input {...register("GithubLink")} />
+                </div>
+            </section>
+            <div className="submissionContainer">
+                <button type="submit">Submit</button>
+            </div>
         </form>
     );
 };
